bab225788e2a2d235cfe83877cb7b36632d5cfbf
vincent <a.mlynarski@exlabs.pl>
2017-09-22 14:53:32 +0200

    Icon play on game item

--- src/app/games/games-list/game-item/game-item.component.ts
+++ src/app/games/games-list/game-item/game-item.component.ts
@@ -3,6 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Game } from '../../shared/game.model';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
+const SHOW_PLAY_TIME = 500;
+
 @Component({
   selector: 'app-game-item',
   templateUrl: './game-item.component.html',
@@ -17,13 +19,45 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
       transition(':leave', [
         animate(300, style({transform: 'translateX(100%)'}))
       ])
+    ]),
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-360deg)' })),
+      transition('rotated => default', animate('400ms ease-out')),
+      transition('default => rotated', animate('400ms ease-in'))
     ])
   ]
 })
 export class GameItemComponent implements OnInit {
   @Input() game: Game;
+  showTimeout: any;
+  showPlayButton: boolean;
 
   constructor() { }
+
+  onMouseEnter() {
+    this.showTimeout = setTimeout(
+      () => {
+        this.showPlay();
+      },
+      SHOW_PLAY_TIME
+    );
+  }
+
+  onMouseLeave() {
+    if (this.showTimeout) {
+      clearTimeout(this.showTimeout);
+    }
+    this.hidePlay();
+  }
+
+  showPlay() {
+    this.showPlayButton = true;
+  }
+
+  hidePlay() {
+    this.showPlayButton = false;
+  }
 
   ngOnInit() {
   }
